91f673203f46fbd4ee03bde2fb68ee508869fecc
VadimS4 <github email address>
2020-04-24 18:44:44 -0700

    initial stages of url shortener

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/:short_url', to: 'link#show'
+  post '/create', to: 'link#create'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+link = Link.create(original_url: 'https://www.linkedin.com/in/vadimstakhnyuk/')
